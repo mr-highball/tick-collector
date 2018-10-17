5607dfd611510bb131459359d7da93a531e45b5d
mr-highball <35942565+mr-highball@users.noreply.github.com>
2018-10-17 14:22:18 -0400

    ui started -started laying out some ui -updated ezthreads

--- ui/main.pas
+++ ui/main.pas
@@ -5,8 +5,8 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  collector;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  ExtCtrls, StdCtrls, CheckLst, collector;
 
 type
 
@@ -16,12 +16,28 @@ type
     configure any additional settings
   *)
   TMainForm = class(TForm)
+    btn_multi: TButton;
+    chk_list_products: TCheckListBox;
+    lbl_list_products: TLabel;
+    pctrl_main: TPageControl;
+    scroll_products: TScrollBox;
+    line_list_products: TShape;
+    ts_products: TTabSheet;
+    ts_auth: TTabSheet;
+    procedure btn_multiClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
+    const
+      BTN_MULTI_START = 'Start';
+      BTN_MULTI_STOP = 'Stop';
+  private
     FCollector: ITickCollector;
+    procedure MultiAction;
+    procedure CheckValidAuth;
   public
-
+    procedure StartCollector;
+    procedure StopCollector;
   end;
 
 var
@@ -36,11 +52,47 @@ implementation
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
   FCollector:=TTickCollectorImpl.Create;
+  StopCollector;
+  CheckValidAuth;
+end;
+
+procedure TMainForm.btn_multiClick(Sender: TObject);
+begin
+  MultiAction;
 end;
 
 procedure TMainForm.FormDestroy(Sender: TObject);
 begin
   FCollector:=nil;
+end;
+
+procedure TMainForm.MultiAction;
+begin
+  if FCollector.State = tsStarted then
+    StopCollector
+  else
+    StartCollector;
+end;
+
+procedure TMainForm.CheckValidAuth;
+begin
+  //todo - checks to see if we have valid auth info if not switches focus
+end;
+
+procedure TMainForm.StartCollector;
+begin
+  //if FCollector.State = tsStopped then
+  //  FCollector.Start;
+  btn_multi.Caption:=BTN_MULTI_START;
+  chk_list_products.Enabled:=False;
+end;
+
+procedure TMainForm.StopCollector;
+begin
+  if FCollector.State = tsStarted then
+    FCollector.Stop;
+  btn_multi.Caption:=BTN_MULTI_STOP;
+  chk_list_products.Enabled:=True;
 end;
 
 end.
